13a45262403fc428a5e4dd1d81758132baf8553b
compmix <ngfelixc@gmail.com>
2016-01-14 22:00:25 -0800

    boarding logic complete, time for debugging

--- p1/boarding/boarding.cpp
+++ p1/boarding/boarding.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 
 
-enum AisleState { EMPTY, NEW, STOR_1, STOR_2, WAITING};
+enum AisleState { EMPTY, NEW, STOR_1, STOR_2, WAITING, FULL};
 /*  Definition of states:
  *
  *  EMPTY: Row is completely empty
@@ -23,6 +23,11 @@ class Passenger {
   public:
     int number;
     char letter;
+    void remove() {
+      number = 0;
+      letter = 0;
+    };
+
 };
 
 class Row {
@@ -30,7 +35,7 @@ class Row {
     int rowID;
     StackAr<char> ABC;
     StackAr<char> DEF;
-    StackAr<Passenger> out;
+    StackAr<char> out;
     Passenger next;
     AisleState state;
     
@@ -87,104 +92,116 @@ int main(int argc, char** argv)
     RowList.enqueue(curRow);
   }
 
-   cerr << curRow.rowID << endl;
 // II. Put passengers where they belong. //
-//1. check row 48
-//6:07 PMFelix2. if not empty, do stuff
-//6:07 PMFelix3. use getFront() on rowList to get the information from row 47
-//6:07 PMFelix3a. check if they have a next Passenger
-//6:08 PMFelix3b. copy nextPassenger from row 47 to row 48
-//
-//
 
+  Row prevRow = RowList.dequeue();
+  int seated = 0;
+  while(seated < 288){
 
-  while(1){
-    Row prevRow;
+    cerr << "ROW state aisle --------------------------------------seated---" << seated << " time: " << clock/5 << endl;
 
     for(int i = 48; i > 0; i--) {       // iterate back to front
-      curRow = RowList.dequeue();
-      Row prevRow;
+      curRow = prevRow;
+      prevRow = RowList.dequeue();
 
       switch(curRow.state) {
         case EMPTY:
-              if(curRow.rowID == 1)     // if row 1 empty, board next passenger in queue
-                  curRow.next = PBoarding.dequeue();
-              else                      // for all other rows get from the previous row
-                prevRow = RowList.getFront();
-              curRow.state = NEW;
+        case FULL:
+              if(curRow.rowID == 1) {    // if row 1 empty, board next passenger in queue
+                  if (!PBoarding.isEmpty()) {       // if end of queue, don't get anything
+                    curRow.next = PBoarding.dequeue();
+                    curRow.state = NEW;
+                  }
+
+              } else {                     // for all other rows get from the previous row
+                if (prevRow.next.number > prevRow.rowID) {
+                  curRow.next = prevRow.next;
+                  curRow.state = NEW;
+                  prevRow.next.remove();
+                  prevRow.state = EMPTY;
+                }
+              }
+
               break;
         case NEW:
               if(curRow.next.number == curRow.rowID)     // if passenger needs to sit here
                 curRow.state = STOR_1;
-              else                                       // else we know next time to get a new passenger
-                curRow.state = EMPTY;
               break;
         case STOR_1:
               curRow.state = STOR_2;
               break;
         case STOR_2:
-              curRow.state = WAITING;
+              if (curRow.next.letter <= 'C'){
+                if (curRow.ABC.isEmpty() || curRow.next.letter > curRow.ABC.top()){ // if row is empty or if passenger to be seated sits closer to aisle than those already seated
+                  curRow.ABC.push(curRow.next.letter);                              // they can just sit down
+                  curRow.next.remove();
+                  seated++;
+                  curRow.state = EMPTY;
+                }
+                else{ // otherwise we need to start making room for them
+                  curRow.out.push(curRow.ABC.topAndPop());                    
+                  curRow.state = WAITING;
+                }
+              }
+              
+              // passenger is D, E, or F
+              else if (curRow.next.letter >= 'D'){
+                if (curRow.DEF.isEmpty() || curRow.next.letter < curRow.DEF.top()){ // if row is empty or if passenger to be seated sits closer to aisle than those already seated
+                  curRow.DEF.push(curRow.next.letter);                              // they can just sit down
+                  curRow.next.remove();
+                  seated++;
+                  curRow.state = EMPTY;
+
+                }
+                else{ // otherwise we need to start making room for them
+                  curRow.out.push(curRow.DEF.topAndPop());                    
+                  curRow.state = WAITING;
+                }
+              }
               break;
         case WAITING:
-              if(curRow.next.letter == ('A' | 'B' | 'C')) {    // check for existing
-                if(curRow.ABC.isEmpty()) {                      // if ABC empty, just fill it
-                  curRow.ABC.push(curRow.next.letter);
-                } else {                                        // if ABC not empty, iterate.
-                    while(!ABC.isEmpty()) {
-                      if(curRow.ABC.top() < curRow.next.letter) { // if the passenger can just sit down
-                        curRow.ABC.push(curRow.next.letter);
-                        break;
-                      }
-                      
-                    }
-                }
 
+              if (curRow.next.letter <= 'C'){
+                if (curRow.ABC.isEmpty() || curRow.next.letter > curRow.ABC.top()){
+                  curRow.ABC.push(curRow.next.letter);   //sit
+                  curRow.next.letter = curRow.out.topAndPop();   //put whoever was in out to go next
+                }
+                else{                         //can't sit, take another out
+                  curRow.out.push(curRow.ABC.topAndPop());
+                }
+              } else if (curRow.next.letter >= 'D') {
+                if (curRow.DEF.isEmpty() || curRow.next.letter < curRow.DEF.top()){
+                  curRow.DEF.push(curRow.next.letter);   //sit
+                  curRow.next.letter = curRow.out.topAndPop();   //put whoever was in out to go next
+                }
+                else{                         //can't sit, take another out
+                  curRow.out.push(curRow.DEF.topAndPop());
+                }
+              }
+              
+              // everyone seated one side of the aisle
+              if (curRow.out.isEmpty() && (curRow.next.letter >= 'C' || curRow.next.letter <= 'D')) {
+                curRow.state = STOR_2;
               }
 
               break;
 
-
       } //switch
 
-
-/*
-          if(curRow.next.number == curRow.rowID){       // if waiting passenger belongs in this row, do stuff
-
-
-
-            // update states
-          } else {
-
-
-            prevRow = RowList.getFront();
-            if (prevRow.next.number != prevRow.rowID) {
-            curRow.next = prevRow.next.number;
-          }
-
-
-          }
-
-*/
-
+      cerr << curRow.rowID << " " << curRow.state << " " << curRow.next.number << curRow.next.letter << endl;
 
       RowList.enqueue(curRow);
    }//for
-
-
-
    clock += 5;
-   break;
+
+
   } //while
+
+
+  cerr << "Back to front: " << clock << endl;
+
+  clock = 0;
 
   return 0;
 }
 
-
-
-/*
-  Row row1, row2, row3, row4, row5, row6, row7, row8, row9, row10,
-      row11, row12, row13, row14, row15, row16, row17, row18, row19, row20,
-      row21, row22, row23, row24, row25, row26, row27, row28, row29, row30,
-      row31, row32, row33, row34, row35, row36, row37, row38, row39, row40,
-      row41, row42, row43, row44, row45, row46, row47, row48;
-*/
